fd488d83894a8f27c172314e903fb5b836071c64
Yogesh <yogesh@Yogeshs-MacBook-Air.local>
2020-02-27 23:39:14 +0530

    Updates

---
+++ precache-manifest.d688e5ab420c83d73471de82ae45cf41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819582b47f91f8d323dcd48626c32473",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2859f8ea1d1cb0648255",
+    "url": "/react-gh-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "69f5ef31a3abb5e5b886",
+    "url": "/react-gh-pages/static/js/2.52e7bb87.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-gh-pages/static/js/2.52e7bb87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2859f8ea1d1cb0648255",
+    "url": "/react-gh-pages/static/js/main.d6a69cd4.chunk.js"
+  },
+  {
+    "revision": "ecfd1d276f9241e07c49",
+    "url": "/react-gh-pages/static/js/runtime-main.d24436ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
